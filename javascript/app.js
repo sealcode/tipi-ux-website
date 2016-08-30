04e42d336cad3c6b913b562d3f479359b6378578
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2016-08-30 18:25:42 +0200

    Layout and a part of design

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+document.getElementById("gdynia-map").style.display = "none";
+document.getElementById("warszawa-map").style.display = "none";
+document.getElementById("poznan-map").style.display = "none";
+
+var gdynia=document.getElementById("gdynia");
+var warszawa=document.getElementById("warszawa");
+var poznan=document.getElementById("poznan");
+
+function showGdynia() {
+	document.getElementById("gdynia-map").style.display = "block";
+	document.getElementById("warszawa-map").style.display = "none";
+	document.getElementById("poznan-map").style.display = "none";
+}
+
+function showWarszawa() {
+	document.getElementById("warszawa-map").style.display = "block";
+	document.getElementById("gdynia-map").style.display = "none";
+	document.getElementById("poznan-map").style.display = "none";
+}
+
+function showPoznan() {
+	document.getElementById("poznan-map").style.display = "block";
+	document.getElementById("gdynia-map").style.display = "none";
+	document.getElementById("warszawa-map").style.display = "none";
+}
+
+gdynia.onclick = showGdynia;
+warszawa.onclick = showWarszawa;
+poznan.onclick = showPoznan;
