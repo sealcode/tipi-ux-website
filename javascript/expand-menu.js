dd730fb909e0593fd5cf989011ca4d44afb16c42
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-10-03 16:14:21 +0200

    use jquery for menu toggle

--- javascript/expand-menu.js
+++ javascript/expand-menu.js
@@ -1,6 +1,15 @@
-let navbar = document.getElementById("navbar");
-let menu   = document.getElementById("expanded-menu");
+var navbar = document.getElementById("navbar");
+var menu   = document.getElementById("expanded-menu");
 
+
+/*
 document.getElementById("expand-menu").addEventListener("click", () => {
   navbar.classList.toggle("navbar-expanded");
+});
+
+*/
+
+$('#expand-menu').click(function() {
+	$("#navbar").toggleClass('navbar-expanded');
+	//console.log($("#navbar").attr('class'));
 });
