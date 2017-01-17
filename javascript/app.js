6abbd0ba92db6e7be15ea9255947145471e336e3
marcin-szczepanski <marcin.szczepanski1995@gmail.com>
2017-01-17 16:10:43 +0100

    Add Kraków to map

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,37 @@
 document.getElementById("gdynia-map").style.display = "none";
 document.getElementById("warszawa-map").style.display = "none";
+document.getElementById("krakow-map").style.display = "none";
 document.getElementById("poznan-map").style.display = "block";
 
 var gdynia=document.getElementById("gdynia");
 var warszawa=document.getElementById("warszawa");
 var poznan=document.getElementById("poznan");
+var krakow=document.getElementById("krakow");
 
 function showGdynia() {
 	document.getElementById("gdynia-map").style.display = "block";
 	document.getElementById("warszawa-map").style.display = "none";
 	document.getElementById("poznan-map").style.display = "none";
+	document.getElementById("krakow-map").style.display = "none";
 }
 
 function showWarszawa() {
 	document.getElementById("warszawa-map").style.display = "block";
 	document.getElementById("gdynia-map").style.display = "none";
 	document.getElementById("poznan-map").style.display = "none";
+	document.getElementById("krakow-map").style.display = "none";
 }
 
 function showPoznan() {
 	document.getElementById("poznan-map").style.display = "block";
+	document.getElementById("gdynia-map").style.display = "none";
+	document.getElementById("warszawa-map").style.display = "none";
+	document.getElementById("krakow-map").style.display = "none";
+}
+
+function showKrakow() {
+	document.getElementById("krakow-map").style.display = "block";
+	document.getElementById("poznan-map").style.display = "none";
 	document.getElementById("gdynia-map").style.display = "none";
 	document.getElementById("warszawa-map").style.display = "none";
 }
@@ -27,3 +39,4 @@ function showPoznan() {
 gdynia.onclick = showGdynia;
 warszawa.onclick = showWarszawa;
 poznan.onclick = showPoznan;
+krakow.onclick = showKrakow;
